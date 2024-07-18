38a7cdcd687883a0d297d15005dd5e198b0b3e04
leonardo <leonardo77ramos@gmail.com>
2024-07-18 19:58:42 -0300

    Aula 18072024 parte 2

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,10 +4,10 @@ namespace EcommerceAPI.Models
     public class Product
     {
         public int id { get; set; }
-        public required string title { get; set; }
-        public required string description { get; set; }
-        public required string? image_url { get; set; }
-        public required int price { get; set; }
-        public required int stock { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string? image_url { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
     }
 }
